7095b432e0944349065929a4de7c2e2c7764a864
jakezappin <jake.zappin@gmail.com>
2017-03-12 00:35:21 -0500

    Added BMI formula, method and properties

--- src/App.js
+++ src/App.js
@@ -17,15 +17,26 @@ class App extends Component {
   }
 
   heightChange(height){
-    this.setState({height: height}, function(){
-      console.log(this.state);
-    });
+    this.setState({height: height}, this.setBMI);
   }
 
   weightChange(weight){
-    this.setState({weight: weight}, function(){
+    this.setState({weight: weight}, this.setBMI);
+  }
+
+  setBMI(){
+    var bmi = ((this.state.weight / this.state.height / this.state.height) * 10000).toFixed(2);
+    this.setState({bmi: bmi, bmiClass: this.getBMIClass(bmi)}, function(){
       console.log(this.state);
-    });
+    })
+  }
+
+  getBMIClass(bmi){
+    if (bmi < 18.5) return "Underweight";
+    if (bmi >= 18.5 && bmi <= 24.9) return "Normal";
+    if (bmi >= 25 && bmi <= 29.9) return "Overweight";
+    if (bmi >= 30) return "Obese"
+
   }
 
   render() {
